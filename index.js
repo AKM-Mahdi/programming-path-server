f7f25469a2db9b6b55fd2494800d774ccafb41da
A K M Mahdi <94472254+AKM-Mahdi@users.noreply.github.com>
2022-11-07 10:40:19 +0600

    4 course added

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const port = process.env.PORT || 5000;
 const cors = require("cors");
 
 const topics = require("./Data/lessons.json");
+const course = require("./Data/courses.json");
 app.use(cors());
 
 app.get("/lessons", (req, res) => {
   res.send(topics);
 });
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log("Example app listening on port ");
 });
 
 app.get("/lessons/:id", (req, res) => {
@@ -18,4 +20,15 @@ app.get("/lessons/:id", (req, res) => {
   const topic = topics.find((item) => item.id === lesson_id);
   console.log(topic);
   res.send(topic);
+});
+
+app.get("/course", (req, res) => {
+  res.send(course);
+});
+
+app.get("/course/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedCourse = topics.filter((item) => item.course_id == id);
+  console.log(selectedCourse);
+  res.send(selectedCourse);
 });
